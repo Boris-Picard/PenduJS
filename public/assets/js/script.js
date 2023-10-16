08c49ece7c6741a9b57a7131a0fa0b07c5ef0e62
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 <picard.boris@gmail.com>
2023-10-16 16:43:46 +0200

    pendu in progress

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,22 +1,117 @@
 /* On commence par une liste de mots random contenu dans un tableau 
 qui doit etre séléctionné aléatoirement */
-const array = ["azerty", "kevin", "deux", "arnaud", "maxime", "aaaeee"];
+const array = ["sorciere",
+"citrouille",
+"fantome",
+"vampire",
+"zombie",
+"momie",
+"chausouris",
+"araignee",
+"toiledaraignee",
+"monstre",
+"costume",
+"balai",
+"potion",
+"crane",
+"os",
+"tombe",
+"cimetiere",
+"nuit",
+"epouvantail",
+"epee",
+"balaivolant",
+"chaudron",
+"bonbon",
+"squelette",
+"maquillage",
+"magie",
+"mystere",
+"sorcellerie",
+"chatnoir",
+"lune",
+"hurlement",
+"chausourisvampire",
+"loupgarou",
+"feufollet",
+"zombieaffame",
+"arbrehante",
+"diable",
+"frisson",
+"epouvante",
+"sombre",
+"effrayant",
+"noir",
+"effetdelumiere",
+"hurleur",
+"chateauhante",
+"masque",
+"ossements",
+"ombre",
+"epine",
+"abomination",
+"sang",
+"malediction",
+"chairputrefiee",
+"chasseurdemonstres",
+"frankenstein",
+"balaiensorcele",
+"clignotant",
+"tombstone",
+"fouet",
+"loup",
+"magicien",
+"enchanteur",
+"citrouilleilluminee",
+"chaudronbouillonnant",
+"chaines",
+"sombreforet",
+"cris",
+"effroi",
+"portail",
+"fete",
+"noirceur",
+"labyrinthe",
+"chaudronfumant",
+"rirediabolique",
+"cicatrice",
+"tetedemort",
+"aiguille",
+"monstrueux",
+"effetsonore",
+"potionmagique",
+"sueurfroide",
+"malediction",
+"morsure",
+"crevette",
+"malefique",
+"lueur",
+"maudit",
+"demon",
+"decadent",
+"poupeeensorcelee",
+"goules",
+"sombresoiree",
+"chateaulugubre",
+"necromancien",
+"maitredestenebres",
+"saignee",
+"mortvivant",
+"loupgarouaffame",
+"princessedestenebres",
+"hurlementdeloup"];
 let randomArray = array[Math.floor(Math.random() * array.length)];
-console.log(randomArray);
 
 // je créer un nouveau tableau de mon mot random que je split lettre par lettre
 let newArraySplit = randomArray.split("");
-console.log(newArraySplit);
 
 // je créer un tableau vide pour l'utilisateur
 let userArray = [];
-console.log(userArray);
 
 // j'ajoute a mon tableau utilisateur des underscores suivant la longueur du mot random dans mon html
 let splitUnderscore = () => {
     for(let letter = 0; letter < newArraySplit.length; letter++ ) {
     userArray[letter] = "_";
-    console.log(userArray);
 };
 };
 
@@ -26,14 +121,40 @@ splitUnderscore();
 const words = document.getElementById("word");
 words.append(userArray.join(" "));
 
-// je récupère la lettre tapé par l'utilisateur que je vais comparer au tableau du mot random et afficher la lettre correspondante si true dans le tableau avec _
-let key = (event) => {
-    event.key;
-    // for()
+// je récupère la lettre tapé par l'utilisateur que je vais comparer au tableau du mot random et afficher la lettre correspondante si true dans le tableau user
+let keys = (event) => {
+    if(newArraySplit.includes(event.key)) {
+        for(let key = 0; key < newArraySplit.length; key++) {
+            if(event.key === newArraySplit[key]) {
+                userArray[key] = newArraySplit[key]; 
+                words.innerHTML = userArray.join(" ");
+            };
+        };     
+    } else {
+        console.log("wrong");
+    };
 };
 
-addEventListener("keydown", key);
+addEventListener("keydown", keys);
 
+//je créer aussi un clavier virtuel 
+const virtualBoard = document.querySelectorAll(".virtualBoard");
+
+virtualBoard.forEach(letter => {
+    letter.addEventListener("click", () => {
+        const letterValue = letter.textContent;
+        if(newArraySplit.includes(letterValue)) {
+            for(let key = 0; key < newArraySplit.length; key++) {
+                if(letterValue === newArraySplit[key]) {
+                    userArray[key] = newArraySplit[key]; 
+                    words.innerHTML = userArray.join(" ");
+                };
+            };     
+        } else {
+            console.log("wrong");
+        };
+    })
+})
 
 
 // on affiche le nombres de lettres attendu pour ce mot random
@@ -53,9 +174,3 @@ est correct elle s'affiche dans le mot a trouver si false le dessin commence a s
 
 // je créer un bouton pour rejouer 
 
-
-
-// if(newArraySplit.find((element) => element === event.key)){
-        
-//     console.log("aaa");
-// };
